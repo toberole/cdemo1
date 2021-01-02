f0389d4cc04026221bc5c9bbbbb2d50608f90cbd
zhouwei441300 <zhouwei441300@163.com>
2021-01-02 12:57:50 +0800

    note

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,28 @@
-#include <iostream>
+﻿#include <iostream>
 
-#include "demo1/Student.h"
+#include <demo1/Student.h>
+#include <demo1/Person.h>
 
 void test1_main() {
 //    Student student;
 //    Student student1 = std::move(student);
-    Student*stu = new Student();
+    Student *stu = new Student();
     Student student2 = std::move(*stu);
+}
+
+void test2_main() {
+    Person p;
+    int i = -1;
+    p.ip = &i;
+    p.print();
+    Person p1 = std::move(p);
+    p.print();
+    p1.print();
 }
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     test1_main();
+    std::cout << "今天天气怎么样" << std::endl;
     return 0;
 }
