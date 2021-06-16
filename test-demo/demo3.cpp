b31c6dcc6a0f2b1240ee47c9ad90e50c0d09c0c9
zhouwei215664 <zhouwei215664@sogou-inc.com>
2021-06-16 10:38:12 +0800

    note

--- test-demo/demo3.cpp
+++ test-demo/demo3.cpp
@@ -1,15 +1,25 @@
-#include<iostream>
+#include <iostream>
 
-void test1(){
-    std::cout<<"test1 ......"<<std::endl;
+void test1()
+{
+    std::cout << "test1 ......" << std::endl;
+    // L表示双字节，也就是每个字符占用两个字节，就是可能出现中文，
+    // R是里面的字符串都不转义
     // 注意中文字符保存 前面需要加个‘L’
     char32_t ch = L'中';
     wchar_t ch1 = L'中';
 }
 
+void test2()
+{
+    int p = 0, &i = p, *t = &p;
+
+    int *ii = &p;
+}
+
 int main(int argc, char const *argv[])
 {
-    std::cout<<"Hello Demo3 ......"<<std::endl;
-   
+    std::cout << "Hello Demo3 ......" << std::endl;
+
     return 0;
 }
